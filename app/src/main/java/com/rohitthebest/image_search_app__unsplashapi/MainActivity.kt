54c670ff8243a57d8e711a3dc00826e376a0749c
Rohit <rkumar8092378845@gmail.com>
2020-10-21 13:17:21 +0530

    initial commit 2

---
+++ app/src/main/java/com/rohitthebest/image_search_app__unsplashapi/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.rohitthebest.image_search_app__unsplashapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
